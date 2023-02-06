1942b92eaad92d373321aa18ac768595169354a5
Mohamed Deiab <63822100+m07med176@users.noreply.github.com>
2023-02-06 12:44:36 +0200

    07-build_android_app_dependencies

--- app/src/main/java/com/example/android/App.java
+++ app/src/main/java/com/example/android/App.java
@@ -1,14 +1,11 @@
 package com.example.android;
 
-import com.google.gson.Gson;
-
 public class App{
 	
 	public String greatingMessage(){
 		return "Hello World";
 	}
 	public static void main(String[] args){
-		Gson gson = new Gson();
 		String message = new App().greatingMessage();
 		System.out.println(message);
 	}
