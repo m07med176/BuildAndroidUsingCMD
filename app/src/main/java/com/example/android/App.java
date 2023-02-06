4166979ff6df06aad655e4f19b041e14de5edd1c
Mohamed Deiab <63822100+m07med176@users.noreply.github.com>
2023-02-06 12:53:11 +0200

    09-build_android_app_launch

--- app/src/main/java/com/example/android/App.java
+++ app/src/main/java/com/example/android/App.java
@@ -8,6 +8,8 @@ public class App extends AppCompatActivity{
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.app);
+		System.out.println("Hello World");
     }
 	
 	
